959f03391010a9a38dc80a2918342cb93f9fba3e
by-sabbir <me@sabbir.dev>
2024-03-07 01:11:01 +0600

    init: echo api scaffold

---
+++ handlers/handler.go
@@ -0,0 +1,50 @@
+package handlers
+
+import (
+	"io"
+	"log/slog"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/labstack/echo/v4"
+)
+
+type Handler struct {
+	Router *echo.Echo
+	Logger *slog.Logger
+	Server *http.Server
+}
+
+func NewHandler() *Handler {
+
+	e := echo.New()
+	l := slog.New(slog.NewJSONHandler(io.MultiWriter(os.Stderr, os.Stdin), nil))
+	h := &Handler{
+		Logger: l,
+	}
+	server := &http.Server{
+		Addr:         ":8080",
+		ReadTimeout:  30 * time.Second,
+		Handler:      e.Server.Handler,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	h.Server = server
+	h.Router = e
+	h.mapRoute()
+	return h
+}
+
+func (h *Handler) Ping(c echo.Context) error {
+
+	c.JSON(http.StatusOK, map[string]string{
+		"ping": "pong",
+	})
+
+	return nil
+}
+
+func (h *Handler) mapRoute() {
+	h.Router.GET("/ping", h.Ping)
+}
---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+
+	"github.com/by-sabbir/echo-scaffold/handlers"
+)
+
+func main() {
+	if err := run(); err != nil {
+		log.Fatalf("could not start server: %+v", err)
+	}
+
+}
+
+func run() error {
+
+	srv := handlers.NewHandler()
+
+	// srv.Logger.Info("server initializing", "host", srv.Server.Addr)
+	return srv.Server.ListenAndServe()
+}
